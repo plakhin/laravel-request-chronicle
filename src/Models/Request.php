d259cbebfdcfc781313dd3c29e6419ede453be34
Stanislav Plakhin <plakhin@gmail.com>
2025-02-01 15:26:56 +0500

    update migration and add `Request` Model

---
+++ src/Models/Request.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace Plakhin\RequestChronicle\Models;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\MassPrunable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
+use Plakhin\RequestChronicle\Enums\HttpMethod;
+
+/**
+ * @property HttpMethod $method
+ * @property string $url
+ * @property array $headers
+ * @property array $payload
+ * @property string $raw
+ * @property array $ips
+ * @property ?Model $model
+ * @property Carbon $created_at
+ * @property array $flatHeaders
+ * @property array $getVariables
+ * */
+class Request extends Model
+{
+    use MassPrunable;
+
+    public const ?string UPDATED_AT = null;
+
+    protected $fillable = [
+        'method',
+        'url',
+        'headers',
+        'payload',
+        'raw',
+        'ips',
+        'model_type',
+        'model_id',
+    ];
+
+    /** @return array<mixed> */
+    protected function casts(): array
+    {
+        return [
+            'method' => HttpMethod::class,
+            'headers' => 'array',
+            'payload' => 'array',
+            'ips' => 'array',
+        ];
+    }
+
+    public function getTable(): string
+    {
+        return config('request-chronicle.table_name');
+    }
+
+    public function prunable(): Builder
+    {
+        return static::where('created_at', '<=', now()->subHours(
+            config('request-chronicle.prune_after_hours', 24 * 7)
+        ));
+    }
+
+    public function model(): MorphTo
+    {
+        return $this->morphTo();
+    }
+
+    /** @return Attribute<array<string>, never> */
+    protected function flatHeaders(): Attribute
+    {
+        return Attribute::make(
+            get: fn (): array => array_map(fn ($header) => $header[0], (array) $this->headers),
+        );
+    }
+
+    /** @return Attribute<array<string>, never> */
+    protected function getVariables(): Attribute
+    {
+        return Attribute::make(
+            get: function (): array {
+                $vars = [];
+                parse_str(urldecode(strval(parse_url($this->url, PHP_URL_QUERY))), $vars);
+
+                return $vars;
+            },
+        );
+    }
+}
